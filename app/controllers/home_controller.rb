5fb37e25263bcabb474b24f547c73d2c7892ddbc
teck-san <tloo0009@student.monash.edu>
2023-09-18 17:14:45 +0800

    added team members pictures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
 
     def index
+        @forecasted = `python3 lib/assets/python/model.py`
+        @data_keys = [
+            'January',
+            'February',
+          'March',
+              'April',
+              'May',
+              'June',
+            ]
+        @data_values = [0, 10, 5, 2, 20, 30, 45]
+
     end
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,5 +5,17 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+document.addEventListener('turbolinks:load', () => {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: JSON.parse(ctx.canvas.dataset.labels),
+      datasets: [{
+        data: JSON.parse(ctx.canvas.dataset.data),
+      }]
+    },
+    });
+  })
 
 export { application }
