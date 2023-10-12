e7a9e7937c21beaeb16f630542530fde2fb9f55b
teck-san <tloo0009@student.monash.edu>
2023-10-12 11:58:26 +0800

    fix images and container

--- app/javascript/dropdown.js
+++ app/javascript/dropdown.js
@@ -34,12 +34,12 @@ document.getElementById("first").addEventListener("click", function(event) {
 
         var char1 = document.getElementById("myChart");
 
-        char1.style.width="1000px";
+        char1.style.width="100%";
         char1.style.height="500px";
 
         var char6 = document.getElementById("myChart6");
 
-        char6.style.width="500px";
+        char6.style.width="100%";
         char6.style.height="200px";
 
     
@@ -66,12 +66,12 @@ document.getElementById("second").addEventListener("click", function(event) {
 
   var char2 = document.getElementById("myChart2")
 
-  char2.style.width="1000px";
+  char2.style.width="100%";
   char2.style.height="500px";
 
   var char7 = document.getElementById("myChart7");
 
-        char7.style.width="500px";
+        char7.style.width="100%";
         char7.style.height="200px";
 
 
@@ -98,12 +98,12 @@ document.getElementById("third").addEventListener("click", function(event) {
 
   var char3 = document.getElementById("myChart3")
 
-  char3.style.width="1000px";
+  char3.style.width="100%";
   char3.style.height="500px";
 
   var char8 = document.getElementById("myChart8");
 
-        char8.style.width="500px";
+        char8.style.width="100%";
         char8.style.height="200px";
 
   
@@ -126,12 +126,12 @@ document.getElementById("forth").addEventListener("click", function(event) {
 
   var char4 = document.getElementById("myChart4")
 
-  char4.style.width="1000px";
+  char4.style.width="100%";
   char4.style.height="500px";
 
   var char9 = document.getElementById("myChart9");
 
-        char9.style.width="500px";
+        char9.style.width="100%";
         char9.style.height="200px";
 
   
@@ -155,12 +155,12 @@ document.getElementById("fifth").addEventListener("click", function(event) {
 
   var char5 = document.getElementById("myChart5")
 
-  char5.style.width="1000px";
+  char5.style.width="100%";
   char5.style.height="500px";
 
   var char10 = document.getElementById("myChart10");
 
-        char10.style.width="500px";
+        char10.style.width="100%";
         char10.style.height="200px";
 
   
